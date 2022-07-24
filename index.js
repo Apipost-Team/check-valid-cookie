67432d18876f27fbe6bbe209890ab28123f69ae5
kuwooo <3257132998@qq.com>
2022-07-24 20:47:10 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const _ = require('lodash'),
+    libUrl = require('url-parse'),
+    libCookie = require('cookie');
+
+function isvalid(url, cookie) {
+    const _uri = new libUrl(url);
+
+    // domain
+    if (_.trim(cookie.domain) && !_.endsWith(_uri.hostname, _.trim(cookie.domain))) {
+        return false;
+    }
+
+    // path
+    if (_.trim(cookie.path) && !_.startsWith((_.endsWith(_uri.pathname, '/') ? _uri.pathname : `${_uri.pathname}/`),
+        cookie.path)) {
+        return false;
+    }
+
+    // expire
+    let expires = new Date(cookie.expires);
+    if (_.isDate(expires) && String(new Date("a111")) != "Invalid Date" && _.lt(expires, new Date())) {
+        return false;
+    }
+
+    // secure
+    if (cookie.secure && _uri.protocol != 'https:') {
+        return false;
+    }
+
+    return {
+        "cookie": libCookie.serialize(cookie.name, cookie.value),
+        "set-cookie": libCookie.serialize(cookie.name, cookie.value, cookie)
+    }
+}
